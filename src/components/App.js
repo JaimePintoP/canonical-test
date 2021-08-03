b4f21f1b9c01b250ea33e2838f7acc463e714343
JaimePintoP <jaimepinto.saxo@gmail.com>
2021-08-03 13:47:28 +0200

    Add axios call

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PostCard from "./PostCard";
 
 const App = () => {
-    return <h1>App Component</h1>
-}
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = () => {
+    axios
+      .get(
+        "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json"
+      )
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <h1>App Component</h1>
+      <PostCard />
+    </div>
+  );
+};
 
 export default App;
